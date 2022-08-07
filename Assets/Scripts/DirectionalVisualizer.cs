caa8f56fe3a477961bc88107400dfa9c2f31f46b
belbeeno <belbeeno@gmail.com>
2022-08-06 19:30:43 -0700

    world diag & debug visualizations

---
+++ Assets/Scripts/DirectionalVisualizer.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))/*, ExecuteInEditMode*/]
+public class DirectionalVisualizer : MonoBehaviour
+{
+    public PlayerMovement target = null;
+    public float lineLength = 5f;
+
+    static Material lineMaterial = null;
+    static void CreateLineMaterial()
+    {
+        if (!lineMaterial)
+        {
+            // Unity has a built-in shader that is useful for drawing
+            // simple colored things.
+            Shader shader = Shader.Find("Hidden/Internal-Colored");
+            lineMaterial = new Material(shader);
+            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+            // Turn on alpha blending
+            lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            // Turn backface culling off
+            lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+            // Turn off depth writes
+            lineMaterial.SetInt("_ZWrite", 0);
+        }
+    }
+
+    private void OnPostRender()
+    {
+        if (ReferenceEquals(null, target))
+        {
+            return;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        CreateLineMaterial();
+        GL.PushMatrix();
+        lineMaterial.SetPass(0);
+
+        Vector3 l = target.transform.position + target.GetMoveDirection(-1f, 0f) * lineLength;
+        Vector3 r = target.transform.position + target.GetMoveDirection(1f, 0f) * lineLength;
+        Vector3 t = target.transform.position + target.GetMoveDirection(0f, 1f) * lineLength;
+        Vector3 b = target.transform.position + target.GetMoveDirection(0f, -1f) * lineLength;
+
+        Vector3 notchX = Vector3.Lerp(l, r, (horizontal + 1f) / 2f);
+        Vector3 notchY = Vector3.Lerp(b, t, (vertical + 1f) / 2f);
+
+        GL.Begin(GL.LINES);
+        {
+            GL.Color(Color.red);
+            GL.Vertex(l);
+            GL.Vertex(r);
+            GL.Vertex(notchX + Vector3.up * 0.25f);
+            GL.Vertex(notchX - Vector3.up * 0.25f);
+
+            GL.Color(Color.green);
+            GL.Vertex(t);
+            GL.Vertex(b);
+            GL.Vertex(notchY + Vector3.left * 0.25f);
+            GL.Vertex(notchY + Vector3.right * 0.25f);
+
+            GL.Color(Color.blue);
+            GL.Vertex(target.transform.position + target.GetMoveDirection(1f, -1f) * lineLength);
+            GL.Vertex(target.transform.position + target.GetMoveDirection(-1f, 1f) * lineLength);
+
+            GL.Vertex(target.transform.position + target.GetMoveDirection(1f, 1f) * lineLength);
+            GL.Vertex(target.transform.position + target.GetMoveDirection(-1f, -1f) * lineLength);
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,26 +15,99 @@ public class PlayerMovement : MonoBehaviour
     public float normSpeed = 6f;
     public float fastSpeed = 30f;
 
+    // Killian method
+    /*
+    public Vector3 GetMoveDirection(float h, float v)
+    {
+        if (Mathf.Approximately(h, 0f) && Mathf.Approximately(v, 0f))
+        {
+            return Vector3.zero;
+        }
+    
+        Camera viewCamera = Camera.main;
+        // Do the move in screenspace.
+        Vector3 screenOrigin = viewCamera.WorldToScreenPoint(transform.position);
+        float screenRight = screenOrigin.x + h * 100f;
+        float screenUp = screenOrigin.y + v * 100f;
+    
+        Ray ray = viewCamera.ScreenPointToRay(new Vector3(screenRight, screenUp));
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+        bool success = groundPlane.Raycast(ray, out float d);
+        Debug.Assert(success);
+        Debug.DrawRay(ray.origin + ray.direction * d, Vector3.up * 10f);
+        // Construct worldspace move direction from orthagonal unit vectors.
+        Vector3 moveVector = (ray.origin + ray.direction * d) - transform.position;
+    
+        return moveVector.normalized;
+    }
+    //*/
+
+    // Worldspace Diag
+    //*
+    public Vector3 GetMoveDirection(float h, float v)
+    {
+        if (Mathf.Approximately(h, 0f) && Mathf.Approximately(v, 0f))
+        {
+            return Vector3.zero;
+        }
+
+        Camera viewCamera = Camera.main;
+        // Do the move in screenspace.
+        Vector3 screenOrigin = viewCamera.WorldToScreenPoint(transform.position);
+        float screenH = screenOrigin.x + 100f;
+        float screenV = screenOrigin.y + 100f;
+
+        Ray rayH = viewCamera.ScreenPointToRay(new Vector3(screenH, screenOrigin.y, screenOrigin.z));
+        Ray rayV = viewCamera.ScreenPointToRay(new Vector3(screenOrigin.x, screenV, screenOrigin.z));
+
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+        bool success = groundPlane.Raycast(rayH, out float hT);
+        success &= groundPlane.Raycast(rayV, out float vT);
+        if (!success)
+        {
+            Debug.LogWarning("PlayerMovement could not cast a ray from the camera " + (viewCamera?.name).ToString() + " to the ground plane of the player.");
+            return Vector3.zero;
+        }
+
+        #region Draw Raycasts
+        //*
+        void DrawX(Vector3 pos)
+        {
+            Debug.DrawLine(pos + new Vector3(-0.5f, 0f, 0.5f), pos + new Vector3(0.5f, 0f, -0.5f));
+            Debug.DrawLine(pos + new Vector3(0.5f, 0f, 0.5f), pos + new Vector3(-0.5f, 0f, -0.5f));
+        }
+
+        Debug.DrawLine(transform.position, rayH.origin + rayH.direction * hT);
+        DrawX(rayH.origin + rayH.direction * hT);
+        Debug.DrawLine(transform.position, rayV.origin + rayV.direction * vT);
+        DrawX(rayV.origin + rayV.direction * vT);
+        //*/
+        #endregion
+
+        Vector3 hPos = ((rayH.origin + rayH.direction * hT) - transform.position).normalized;
+        Vector3 vPos = ((rayV.origin + rayV.direction * vT) - transform.position).normalized;
+        vPos *= (Vector3.Dot(viewCamera.transform.forward, vPos) > 0f ? 1f : -1f);
+        // Construct worldspace move direction from orthagonal unit vectors.
+        Vector3 moveVector = hPos * h + vPos * v;
+
+        return moveVector.normalized;
+    }
+    //*/
+
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        direction = new Vector3(horizontal, 0f, vertical).normalized;
-        camAngle = Camera.main.transform.eulerAngles.y;
-
-        if (direction.magnitude >= 0.1f)
+        // Raycast to a vertical plane defined by the player's current position to get the worldspace equivalent point.
+        // Get input.
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector3 moveVector = GetMoveDirection(horizontal, vertical);
+        if (moveVector.sqrMagnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camAngle;
-            playerMesh.transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-            
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir * speed * Time.deltaTime);
-
-            Debug.DrawRay(transform.position, moveDir * 5, Color.yellow, .01f); // draw the debug ray
+            controller.Move(moveVector * speed * Time.deltaTime);
+            Debug.DrawRay(transform.position, moveVector * 5, Color.yellow, .01f); // draw the debug ray
         }
-
-        DebugLoop(); // just cordoning off debug functions into their own zone
+        DebugLoop();
     }
 
     private void DebugLoop()
@@ -49,8 +122,16 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
-        // nothing here rn
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position + GetMoveDirection(1f, 0f) * speed, transform.position + GetMoveDirection(-1f, 0f) * speed);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position + GetMoveDirection(0f, -1f) * speed, transform.position + GetMoveDirection(0f, 1f) * speed);
+        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position + GetMoveDirection(-1f, 1f) * speed, transform.position + GetMoveDirection(1f, -1f) * speed);
+        Gizmos.DrawLine(transform.position + GetMoveDirection(1f, 1f) * speed, transform.position + GetMoveDirection(-1f, -1f) * speed);
     }
+
 }
